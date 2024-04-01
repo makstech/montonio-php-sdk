0442128335625094790586b2daa34d4b97460f0f
Max <maxslotov@icloud.com>
2024-04-02 02:37:01 +0300

    test: add StoresClient integration tests

---
+++ tests/Integration/Clients/StoresClientTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\Clients;
+
+use Tests\BaseTestCase;
+
+class StoresClientTest extends BaseTestCase
+{
+    public function testGetPaymentMethods()
+    {
+        $methods = $this->getMontonioClient()->stores()->getPaymentMethods();
+
+        $this->assertIsArray($methods['paymentMethods']['paymentInitiation']['setup']['LV']['paymentMethods']);
+    }
+}
