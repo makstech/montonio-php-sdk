7ef5f58b703eb2c417b1bb2dd98284b0f340725f
Max <maxslotov@icloud.com>
2024-04-02 02:07:24 +0300

    test: add more MontonioClient tests

--- tests/Unit/MontonioClientTest.php
+++ tests/Unit/MontonioClientTest.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use Montonio\Clients\OrdersClient;
+use Montonio\Clients\StoresClient;
 use Montonio\Structs\OrderData;
 use stdClass;
 use Tests\BaseTestCase;
 
 class MontonioClientTest extends BaseTestCase
 {
-    public function testJwt()
+    public function testJwt(): void
     {
         $data = new OrderData();
         $data->setLocale('lv');
@@ -22,5 +24,27 @@ class MontonioClientTest extends BaseTestCase
         $this->assertNotEmpty($token);
         $this->assertInstanceOf(stdClass::class, $decoded);
         $this->assertSame('lv', $decoded->locale);
+    }
+
+    public function testGetOrdersClient(): void
+    {
+        $data = new OrderData();
+        $data->setLocale('lv');
+
+        $client = $this->getMontonioClient()->orders();
+
+        $this->assertInstanceOf(OrdersClient::class, $client);
+
+        $token = $client->generateToken($data->toArray());
+        $decoded = $client->decodeToken($token);
+
+        $this->assertSame(ACCESS_KEY, $decoded->accessKey, 'Access Key was not passed from parent client');
+    }
+
+    public function testGetStoresClient(): void
+    {
+        $client = $this->getMontonioClient()->stores();
+
+        $this->assertInstanceOf(StoresClient::class, $client);
     }
 }
