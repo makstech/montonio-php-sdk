3373698d8efcbd997fe4225af954fbd4d68b68e5
Max Slotov <max@slotov.dev>
2026-04-12 19:17:03 +0300

    feat: enhance payment links with get method and missing fields (#21)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,5 +7,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-define('ACCESS_KEY', getenv('ACCESS_KEY') ?: '');
-define('SECRET_KEY', getenv('SECRET_KEY') ?: str_repeat('x', 32));
+define('ACCESS_KEY', $_ENV['ACCESS_KEY'] ?? getenv('ACCESS_KEY') ?: '');
+define('SECRET_KEY', $_ENV['SECRET_KEY'] ?? getenv('SECRET_KEY') ?: str_repeat('x', 32));
